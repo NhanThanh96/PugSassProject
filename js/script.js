cb04ec3ae0b0d137c9f7cfc7f4eff01a2e7a358d
NhanThanh96 <thanhnhan030796@gmail.com>
2019-03-18 22:33:00 +0700

    edit effect banner

--- js/script.js
+++ js/script.js
@@ -1,8 +1,29 @@
 $(document).ready(function(){
+    
+        window.setTimeout(function () {
+            // $(".banner .banner__author").animate({
+            //     opacity: '1',
+            // }, "slow");
+            // $(".banner .banner__title").animate({
+            //     opacity: '1',
+            // }, "slow");
+            // $(".banner .banner__paragraph").animate({
+            //     opacity: '1',
+            // }, "slow");
+            // $(".banner .banner__link ").animate({
+            //     opacity: '1',
+            // }, "slow");
+            $(".banner__content").children().each(function(i) {
+                $(this).delay(300 * i).fadeIn(500);
+            });
+        }, 2000);
 
       $(document).on('click', function(event) {
         if (!$(event.target).closest('.navbar-nav').length) {
-            $(".dropdown-menu").removeClass("show");
+            $(".dropdown-menu").removeClass("shown");
+            $(".dropdown-menu").slideUp(250);
+            $(".dropright-menu").removeClass("shown");
+            $(".dropright-menu").slideUp(150);
         }
       });
 
@@ -36,25 +57,25 @@ $(document).ready(function(){
     $(".dropright-menu > .nav-item > a").on("click", function(){
       $(this).parents(".navbar-nav").find(".dropright-menu > .nav-item > a.active").removeClass("active");
       $(this).addClass("active");
-      $(this).parent().parent().removeClass("show");
+      $(this).parent().parent().removeClass("shown");
+      $(this).parent().parent().slideUp(250);
 
   });
 
+// dropdown menu
   $(".dropdown > a").on("click", function(e){
     e.preventDefault();
   
-    if($(this).next().hasClass("show")) {
-        $(this).next().removeClass("show");
-        // $(this).next().slideUp(350);
+    if($(this).next().hasClass("shown")) {
+        $(this).next().removeClass("shown");
+        $(this).next().slideUp(250);
     }
     else {
 
-        $(this).parents(".navbar-nav").find(".dropdown-menu").removeClass("show"); //remove all //remove all
-        // $(this).parents(".navbar-nav").find(".dropdown-menu").slideUp(350);
-        $(this).parents(".navbar-nav").find(".dropright-menu").removeClass("show");
-        // $(this).parents(".navbar-nav").find(".dropdown-menu").slideUp(); // slide up all
-        $(this).next().toggleClass("show");
-        // $(this).next().slideToggle(350);
+        $(this).parents(".navbar-nav").find(".dropdown-menu").removeClass("shown"); //remove all //remove all
+        $(this).parents(".navbar-nav").find(".dropdown-menu").slideUp(250);
+        $(this).next().toggleClass("shown");
+        $(this).next().slideToggle(250);
     }
   });
   //   $(".navbar-nav > .nav-item.dropdown > a").click(function(){
@@ -62,21 +83,19 @@ $(document).ready(function(){
   //       $(this).parent().find(".dropdown-menu").toggleClass("show");
   //   });
 
+//   dropright menu
     $(".dropright > a").on("click", function(e){
       e.preventDefault();
     
-      if($(this).next().hasClass("show")) {
-          $(this).next().removeClass("show");
-
-          // $(this).next().slideUp(350);
-          // $(this).removeClass("show");
+      if($(this).next().hasClass("shown")) {
+          $(this).next().removeClass("shown");
+          $(this).next().slideUp(150);
       }
       else {
-        $(this).parents(".dropdown-menu").find(".dropright-menu").removeClass("show"); //remove all //remove all
-        // $(this).parents(".dropdown-menu").find(".dropright-menu").slideUp(350);
-        // $(this).parents(".dropdown-menu").find(".dropright-menu").slideUp(); // slide up all
-        $(this).next().toggleClass("show");
-        // $(this).next().slideToggle(350);
+        $(this).parents(".dropdown-menu").find(".dropright-menu").removeClass("shown"); //remove all //remove all
+        $(this).parents(".dropdown-menu").find(".dropright-menu").slideUp(150);
+        $(this).next().toggleClass("shown");
+        $(this).next().slideToggle(150);
       }
     });
   //   $(".navbar-nav > .nav-item.dropdown > ul.dropdown-menu > .nav-item.dropright > a").click(function(){
